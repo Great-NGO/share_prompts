1ada73de39f5bc72ffb4ae3ba29bd3c26baa55eb
Great Nwamadi <69170132+Great-NGO@users.noreply.github.com>
2023-07-05 14:15:50 +0100

    3rd commit - Implement CRUD functionality posts, used NextAuth with Google API for authentication

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -4,6 +4,10 @@ import GoogleProvider from 'next-auth/providers/google';
 import User from "@models/user";
 import { connectToDB } from "@utils/database";
 
+
+// Connect to the Database
+// connectToDB();
+
 const handler = NextAuth({
     providers: [
         GoogleProvider({
@@ -11,38 +15,48 @@ const handler = NextAuth({
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         })
     ],
-    async session({ session }) {
-        const sessionUser = await User.findOne({
-            email: session.user.email
-        })
+    callbacks: {
+        //Session Callback
+        async session({ session }) {
+            const sessionUser = await User.findOne({
+                email: session.user.email
+            })
 
-        session.user.id = sessionUser._id.toString();
-    },
-    async signIn({ profile }) {
-        try {
-            
-            await connectToDB();
+            session.user.id = sessionUser._id.toString();
 
-            // Check if a user already exists
-            const userExists = await User.findOne({
-                email: profile.email
-            });
+            return session;
+        },
+        async signIn({ profile }) {
+            // Sign in callback
+            try {
 
-            // If not, create a new user and save to db
-            if(!userExists) {
-                await User.create({
-                    email: profile.email,
-                    username: profile.name.replace(" ", "").toLowerCase(),
-                    image: profile.picture
-                })
+                // Connect to the database
+                await connectToDB();
+
+                console.log("PROFILE :", profile);
+
+                // Check if a user already exists
+                const userExists = await User.findOne({
+                    email: profile.email
+                });
+
+                // If not, create a new user and save to db
+                if (!userExists) {
+                    await User.create({
+                        email: profile.email,
+                        username: profile.name.replace(/ /g, "").toLowerCase(),
+                        image: profile.picture
+                    })
+                }
+                return true;
+
+            } catch (error) {
+                console.log("Error checking if user exists: ", error);
+                return false;
             }
-            return true;
-
-        } catch (error) {
-            console.log(error);
-            return false;
         }
     }
+
 })
 
 export { handler as GET, handler as POST };
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -5,17 +5,19 @@ import { useEffect, useState } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
-  const isUserLoggedIn = true;
+  const { data: session } = useSession();
 
   const [providers, setProviders] = useState(null);
   const [toggleDropDown, setToggleDropDown] = useState(false);
 
   useEffect(() => {
-    const setProviders = async () => {
+    const setUpProviders = async () => {
       const response = await getProviders();
 
       setProviders(response);
     };
+
+    setUpProviders()
   }, []);
 
   return (
@@ -31,9 +33,11 @@ const Nav = () => {
         <p className="logo_text">Promptopia</p>
       </Link>
 
-      {/* Mobile Navigation */}
+      {/* { alert(session?.user)} */}
+
+      {/* Desktop Navigation */}
       <div className="sm:flex hidden">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex gap-3 md:gap-5">
             <Link href="/create-prompt" className="black_btn">
               Create Post
@@ -45,7 +49,7 @@ const Nav = () => {
 
             <Link href="/profile">
               <Image
-                src="/assets/images/logo.svg"
+                src={session?.user.image}
                 width={37}
                 height={37}
                 className="rounded-full"
@@ -72,10 +76,10 @@ const Nav = () => {
 
       {/* Mobile Navigation */}
       <div className="sm:hidden flex relative">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex">
             <Image
-              src="/assets/images/logo.svg"
+              src={session?.user.image}
               width={37}
               height={37}
               className="rounded-full"
