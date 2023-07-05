8e13917b2b1fc39536fb7abe389e066a25a58879
Great Nwamadi <69170132+Great-NGO@users.noreply.github.com>
2023-07-05 15:39:51 +0100

    Updated meta

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,7 +3,7 @@ import Provider from "@components/Provider";
 import "@styles/globals.css";
 
 export const metadata = {
-  title: "Promptopia",
+  title: "NGO-Tech Promptopia",
   description: "Discover & Share AI Prompts",
 };
 
